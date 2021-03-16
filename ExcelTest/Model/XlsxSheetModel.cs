f5103c91c75f26ad90e92e208ea15a0d755d313f
TakaoKoshi <koshi@digitaldolphins.jp>
2021-03-16 17:52:52 +0900

    test

---
+++ ExcelTest/Model/XlsxSheetModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelTest.Model
+{
+	public class XlsxSheetModel
+	{
+		public string SheetName{
+			get;
+			set;
+		}
+		public int SheetIndex{
+			get;
+			set;
+		}
+	}
+}
---
+++ ExcelTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using ExcelTest.Model;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelTest
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Count() == 0 )
+			{
+				Console.WriteLine("ExcelTest.exe XlsxFilePathA.xls XlsxFilePathB.xls");
+				return;
+			}
+
+			var argsFiles = args;
+			foreach (var record in argsFiles)
+			{
+				Console.WriteLine("\r\n");
+				var XlsFilePath = record;
+
+				var xlsxsheetmodel = new XlsxSheetModel();
+
+				///bookよみこみ
+				var book = WorkbookFactory.Create(XlsFilePath);
+				xlsxsheetmodel.SheetName = "SetPrint";
+
+				List<string> sheetNameList = new List<string>();
+				for (int i = 0; i < book.NumberOfSheets; i++)
+				{
+					// シートオブジェクトを取得
+					ISheet sht = book.GetSheetAt(i);
+					// シート名を取得して、リストに保存
+					sheetNameList.Add(sht.SheetName);
+					var isHidden = book.IsSheetHidden(i);
+					Console.WriteLine($"シート名:{sht.SheetName} 隠れているか：{isHidden.ToString()} index:{i}");
+				}
+				Console.WriteLine(string.Join(",", sheetNameList));
+				var sheet = book.GetSheet(xlsxsheetmodel.SheetName);
+				
+				//sheet.ForceFormulaRecalculation = true;
+
+				//印刷させたいシートを設定
+				xlsxsheetmodel.SheetIndex = book.GetSheetIndex(xlsxsheetmodel.SheetName);
+
+				Console.WriteLine($"index:{xlsxsheetmodel.SheetIndex} XlsxFileName{XlsFilePath} ");
+			}
+			
+			return;
+		}
+	}
+}
